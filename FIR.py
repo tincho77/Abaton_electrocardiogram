3ebdd1526260b06d3867f8bc693f314b116f9140
tincho77 <rosas.martin.exequiel@gmail.com>
2021-07-21 10:02:46 -0300

    add FIR filter of EKG signal

---
+++ FIR.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import pi
+import scipy.fftpack as sf
+import scipy.signal as sig
+
+plt.close('all')
+
+# Genarate signal (change for te EKG signal of MOD-EKG board)
